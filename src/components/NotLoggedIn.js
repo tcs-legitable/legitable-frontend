f025f1f4d80dc1b4d08e50434d2a7c0ca73c05df
byronwang93 <byronwang123919@gmail.com>
2024-05-09 14:11:31 -0700

    ANIMATION

--- src/components/NotLoggedIn.js
+++ src/components/NotLoggedIn.js
@@ -1,6 +1,6 @@
-import { Box, Button, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Image, Text, Fade } from "@chakra-ui/react";
 import { signInWithPopup } from "firebase/auth";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SignedInContext } from "../App";
 import { auth, provider } from "../firebase/firebase";
 import { addUser } from "../firebase/helpers";
@@ -9,6 +9,7 @@ import googleLogo from "../assets/images/google-logo.png";
 
 const NotLoggedIn = () => {
   const { setValue } = useContext(SignedInContext);
+  const [show, setShow] = useState(false);
 
   const handleClick = async () => {
     signInWithPopup(auth, provider)
@@ -24,54 +25,81 @@ const NotLoggedIn = () => {
 
   useEffect(() => {
     setValue(localStorage.getItem("user-uid"));
+    let timer = setTimeout(() => setShow(true), 500);
+    return () => clearTimeout(timer);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <Box w="100%" h="100%">
-      <Box margin="30px" bgColor="blue" w="fit-content">
+    <Box w="100%" h="90vh">
+      <Box margin="30px" w="fit-content">
         <Logo width="50px" />
       </Box>
-      <VStack textAlign="center">
-        <Box
-          w={{ base: "380px", md: "800px" }}
-          fontWeight="bold"
-          fontSize={{ base: "34px", md: "40px" }}
-        >
-          <Text as="span">Welcome to </Text>
-          <Text color="#808080" as="span">
-            Legitable
+      <Box
+        pt="20px"
+        display="flex"
+        flexDir="column"
+        alignItems="center"
+        textAlign="center"
+        w="100%"
+        maxH="100vh"
+      >
+        <Fade in={show} transition={{ enter: { duration: 1.5 } }}>
+          <Box
+            w={{ base: "380px", md: "800px" }}
+            fontWeight="bold"
+            fontSize={{ base: "34px", md: "40px" }}
+          >
+            <Text as="span">Welcome to </Text>
+            <Text color="#808080" as="span">
+              Legitable
+            </Text>
+            <Text as="span">
+              : A Gateway to Legitimate Connections for Introverts
+            </Text>
+          </Box>
+        </Fade>
+        <Fade in={show} transition={{ enter: { duration: 1.5, delay: 0.5 } }}>
+          <Text
+            w={{ base: "370px", md: "630px" }}
+            pt={{ base: "35px", md: "50px" }}
+            fontSize={{ base: "22px", md: "30px" }}
+          >
+            Harness the power of trusted networks to spotlight talent within
+            every circle
           </Text>
-          <Text as="span">
-            : A Gateway to Legitimate Connections for Introverts
+        </Fade>
+        <Fade in={show} transition={{ enter: { duration: 1.5, delay: 1 } }}>
+          <Text
+            fontSize={{ base: "29px", md: "44px" }}
+            pt={{ base: "35px", md: "50px" }}
+          >
+            Join. Curate. Share.
           </Text>
-        </Box>
-        <Text
-          w={{ base: "370px", md: "630px" }}
-          pt="40px"
-          fontSize={{ base: "22px", md: "30px" }}
-        >
-          Harness the power of trusted networks to spotlight talent within every
-          circle
-        </Text>
-        <Text fontSize={{ base: "29px", md: "44px" }} pt="40px">
-          Join. Curate. Share.
-        </Text>
-        <Button
-          mt="50px"
-          textColor="#EAEAEA"
-          _hover={{
-            bgColor: "#3C3C3C",
-          }}
-          bgColor="#545454"
-          onClick={handleClick}
-          p="20px"
-          py="30px"
-        >
-          <Image boxSize={10} color="pink" bgColor="inherit" src={googleLogo} />
-          Sign In With Google
-        </Button>
-      </VStack>
+        </Fade>
+        <Fade in={show} transition={{ enter: { duration: 1.5, delay: 1.5 } }}>
+          <Button
+            mt={{ base: "40px", md: "50px" }}
+            textColor="#EAEAEA"
+            _hover={{
+              bgColor: "#3C3C3C",
+            }}
+            bgColor="#545454"
+            onClick={handleClick}
+            p="20px"
+            py="30px"
+            fontWeight="medium"
+          >
+            <Image
+              boxSize={10}
+              color="pink"
+              bgColor="inherit"
+              src={googleLogo}
+            />
+            Sign In With Google
+          </Button>
+        </Fade>
+      </Box>
     </Box>
   );
 };
