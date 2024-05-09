24549044e0ffcc0fbcbc002664d054d84ca2bef5
byronwang93 <byronwang123919@gmail.com>
2024-05-09 13:36:30 -0700

    initial revamp of home page landing

--- src/components/NotLoggedIn.js
+++ src/components/NotLoggedIn.js
@@ -1,9 +1,11 @@
-import { Box, Button, VStack } from "@chakra-ui/react";
+import { Box, Button, Image, Text, VStack } from "@chakra-ui/react";
 import { signInWithPopup } from "firebase/auth";
 import React, { useContext, useEffect } from "react";
 import { SignedInContext } from "../App";
 import { auth, provider } from "../firebase/firebase";
 import { addUser } from "../firebase/helpers";
+import { ReactComponent as Logo } from "../assets/images/tcs-logo.svg";
+import googleLogo from "../assets/images/google-logo.png";
 
 const NotLoggedIn = () => {
   const { setValue } = useContext(SignedInContext);
@@ -26,9 +28,49 @@ const NotLoggedIn = () => {
   }, []);
 
   return (
-    <Box>
-      <VStack>
-        <Button onClick={handleClick} paddingTop='20px'>Log in with google</Button>
+    <Box w="100%" h="100%">
+      <Box margin="30px" bgColor="blue" w="fit-content">
+        <Logo width="50px" />
+      </Box>
+      <VStack textAlign="center">
+        <Box
+          w={{ base: "380px", md: "800px" }}
+          fontWeight="bold"
+          fontSize={{ base: "34px", md: "40px" }}
+        >
+          <Text as="span">Welcome to </Text>
+          <Text color="#808080" as="span">
+            Legitable
+          </Text>
+          <Text as="span">
+            : A Gateway to Legitimate Connections for Introverts
+          </Text>
+        </Box>
+        <Text
+          w={{ base: "370px", md: "630px" }}
+          pt="40px"
+          fontSize={{ base: "22px", md: "30px" }}
+        >
+          Harness the power of trusted networks to spotlight talent within every
+          circle
+        </Text>
+        <Text fontSize={{ base: "29px", md: "44px" }} pt="40px">
+          Join. Curate. Share.
+        </Text>
+        <Button
+          mt="50px"
+          textColor="#EAEAEA"
+          _hover={{
+            bgColor: "#3C3C3C",
+          }}
+          bgColor="#545454"
+          onClick={handleClick}
+          p="20px"
+          py="30px"
+        >
+          <Image boxSize={10} color="pink" bgColor="inherit" src={googleLogo} />
+          Sign In With Google
+        </Button>
       </VStack>
     </Box>
   );
