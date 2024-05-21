278f42465083a17b595a82b1432712eba60e7355
byronwang93 <byronwang123919@gmail.com>
2024-05-21 16:39:13 -0700

    button styling

--- src/components/WaitlistLanding.js
+++ src/components/WaitlistLanding.js
@@ -1,11 +1,47 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Box, Button, Flex, HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import StupaidLogo from "../assets/images/stupaid-logo.svg";
+import TalentButtonIcon from "../assets/images/talent-button-icon.svg";
+import StudentButtonIcon from "../assets/images/student-button-icon.svg";
 
 const WaitlistLanding = () => {
   return (
-    <Flex>
-      <Image src={StupaidLogo} />
+    <Flex flexDir="column" alignItems="center">
+      <Image w="300px" src={StupaidLogo} />
+      <Text pt="20px" fontSize="22px" color="#969696">
+        Where <Box as="span">student creatives</Box> become{" "}
+        <Box as="span">legit.</Box>{" "}
+      </Text>
+      <HStack pt="30px">
+        <Button
+          border="1px solid"
+          p="23px"
+          borderRadius="25px"
+          color="#fafafa"
+          bg="transparent"
+          fontWeight="thin"
+          _hover={{
+            bgGradient: "linear(to-r, #4b4b4b 20%, #0C0C0C 80%)",
+          }}
+        >
+          <Image bgColor="inherit" mr="10px" src={TalentButtonIcon} />
+          I'm looking for talent
+        </Button>
+        <Button
+          border="1px solid"
+          p="23px"
+          borderRadius="25px"
+          color="#fafafa"
+          bg="transparent"
+          fontWeight="thin"
+          _hover={{
+            bgGradient: "linear(to-r, #4b4b4b 20%, #0C0C0C 80%)",
+          }}
+        >
+          <Image bgColor="inherit" mr="10px" src={StudentButtonIcon} /> I'm a
+          student
+        </Button>
+      </HStack>
     </Flex>
   );
 };
--- src/components/WaitlistLandingLayout.js
+++ src/components/WaitlistLandingLayout.js
@@ -1,16 +1,16 @@
-import { Box, Flex, HStack, Image, Link, Text } from "@chakra-ui/react";
+import { Flex, HStack, Image, Link, Text } from "@chakra-ui/react";
 import React from "react";
 import StupaidTCSLogo from "../assets/images/stupaid-tcs-logo.svg";
 
 const WaitlistLandingLayout = ({ children }) => {
   return (
     <Flex direction="column" alignItems="center">
-      <HStack my="100px">
+      <HStack mt="100px" mb="130px">
         <Text color="#969696">Created by</Text>
         <Image src={StupaidTCSLogo} />
       </HStack>
       {children}
-      <Link my="100px" href="mailto:team@thecreativesolution.ca">
+      <Link mt="130px" mb="100px" href="mailto:team@thecreativesolution.ca">
         <Text textDecoration="underline" color="#969696">
           team@thecreativesolution.ca
         </Text>
