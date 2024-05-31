48c9fbd08e8231dd07e1b79065fd74b13ab7be56
byronwang93 <byronwang123919@gmail.com>
2024-05-31 10:38:31 -0700

    gmail sign in

--- src/components/landing-page-components/LandingSignIn.js
+++ src/components/landing-page-components/LandingSignIn.js
@@ -1,7 +1,53 @@
+import { Button, Flex, Image, Text, VStack } from "@chakra-ui/react";
 import React from "react";
+import StupaidLogo from "../../assets/landing-page-images/stupaid-logo-small.svg";
+import GmailArrow from "../../assets/landing-page-images/continue-w-gmail-arrow-black.svg";
 
-const LandingSignIn = () => {
-  return <div>LandingSignIn</div>;
+const LandingSignIn = ({ goNext }) => {
+  return (
+    <Flex
+      zIndex="0"
+      bg="transparent"
+      flexDir="column"
+      alignItems="center"
+      pb="0px"
+    >
+      <Image
+        bg="transparent"
+        w={{ base: "200px", mdLg: "150px" }}
+        src={StupaidLogo}
+      />
+      <VStack spacing="0px" fontSize="20px">
+        <Text pt="15px">Sign in with your Gmail to begin</Text>
+      </VStack>
+      <Flex
+        justifyContent="center"
+        w="100%"
+        flexDir={{ base: "column", mdLg: "row" }}
+        bg="transparent"
+        pt="25px"
+      >
+        <Button
+          border="1px solid"
+          p="23px"
+          borderRadius="25px"
+          color="#fafafa"
+          bgColor="#0c0c0c"
+          fontWeight="regular"
+          _hover={{
+            backgroundPosition: "left bottom",
+          }}
+          _active={{
+            backgroundPosition: "left bottom",
+          }}
+          onClick={() => goNext()}
+        >
+          Continue with Gmail
+          <Image pl="10px" src={GmailArrow} />
+        </Button>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default LandingSignIn;
