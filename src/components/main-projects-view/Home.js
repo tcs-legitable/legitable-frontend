37031b9b42751b586ef5a1726e8555ae5840c200
Alan Wang <alanwang123919@gmail.com>
2024-06-02 17:19:00 -0700

    Created home page for project page + created navbar

---
+++ src/components/main-projects-view/Home.js
@@ -0,0 +1,16 @@
+import { Box, Image, Text } from '@chakra-ui/react'
+import React from 'react'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    <Box>
+        <Navbar/>
+        <Text>
+          hi
+        </Text>
+    </Box>
+  )
+}
+
+export default Home
