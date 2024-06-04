b7a737ebd8f24a7af63471270a237d5e78bbce84
byronwang93 <byronwang123919@gmail.com>
2024-06-04 13:54:21 -0700

    initialized student-landing page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import EndorseesPage from "./components/EndorseesPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import theme from "./theme/theme";
 import GetStartedLandingPage from "./components/landing-page-components/GetStartedLandingPage";
-import Home from "./components/main-projects-view/Home";
+import Home from "./components/organization-view-components/Home";
+import StudentLanding from "./components/student-view-components/StudentLanding";
 
 export const SignedInContext = createContext();
 
@@ -23,7 +24,8 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/landing" element={<GetStartedLandingPage />} />
               <Route path="/endorsees/:userId" element={<EndorseesPage />} />
-              <Route path="/home" element={<Home />}/>
+              <Route path="/home" element={<Home />} />
+              <Route path="/projects" element={<StudentLanding />} />
             </Routes>
           </Router>
         </VStack>
---
+++ src/components/organization-view-components/Creatives.js
@@ -0,0 +1,40 @@
+import { Flex } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import CreativeCard from './CreativeCard'
+import { getAllUsers } from './../../firebase/helpers'
+
+const Creatives = () => {
+    const [creativesList, setCreativesList] = useState([]);
+    
+    useEffect(() => {
+        const fetchData = async () => {
+          const users = await getAllUsers();
+          setCreativesList(users);
+        };
+        fetchData();
+      }, []);
+
+  return (
+    <Flex
+        display='flex'
+        flexDirection='column'
+    >
+        {creativesList.map((creative, index) => (
+        <CreativeCard
+          key={index}
+          photo_url={creative.photo_url}
+          city={creative.city}
+          country={creative.country}
+          school={creative.school}
+          projectPref={creative.projectPref}
+          full_name={creative.full_name}
+          email={creative.email}
+          skills={creative.skills}
+          website={creative.website || "https://www.stupaid.work/"}
+        />
+      ))}
+    </Flex>
+  )
+}
+
+export default Creatives
