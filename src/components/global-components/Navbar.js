95730933fa36cab0d885ca2b216aa22bea0e2782
byronwang93 <byronwang123919@gmail.com>
2024-06-05 17:09:56 -0700

    finished navbar - student view

--- src/components/global-components/Navbar.js
+++ src/components/global-components/Navbar.js
@@ -1,46 +1,113 @@
-import React from 'react';
+import React, { useContext } from 'react';
 // import StupaidLogo from './../../assets/images/stupaid-logo.svg';
 import StupaidLogo from './../../assets/landing-page-images/stupaid-logo-main.svg';
-import { Box, Button, Image } from '@chakra-ui/react';
+import { Box, Button, Flex, Image, Text } from '@chakra-ui/react';
+import { SignedInContext } from '../../App';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const { value } = useContext(SignedInContext);
+  const navigate = useNavigate();
+
   return (
     <Box
+      w="100%"
       display="flex"
       flexDirection="row"
       justifyContent="space-between"
-      paddingTop="1.5vh"
+      py="30px"
       paddingLeft="1.5vw"
       paddingRight="1.5vw"
     >
-      <Image w={{ base: '200px', mdLg: '150px' }} src={StupaidLogo} />
+      <Flex alignItems="center">
+        <Image
+          mx="30px"
+          w={{ base: '150px', mdLg: '120px' }}
+          src={StupaidLogo}
+        />
+        <Text fontWeight="thin" fontSize="20px" color="#555555">
+          Hi,{' '}
+          <Box as="span" textDecoration="underline 1.3px">
+            {value === {} ? 'test_user' : value?.name}
+          </Box>
+        </Text>
+      </Flex>
 
-      <Box className="button-container">
-        <Button border="1px solid" p="23px" borderRadius="25px">
-          Post a project
-        </Button>
-
-        <Button
-          border="1px solid"
-          p="23px"
-          borderRadius="25px"
-          color="#fafafa"
-          bgColor="#0c0c0c"
-          fontWeight="regular"
-          _hover={{
-            backgroundPosition: 'left bottom',
-          }}
-          _active={{
-            backgroundPosition: 'left bottom',
-          }}
+      {value?.type === 'student' ? (
+        <Flex
+          mr="40px"
+          gap="15px"
+          className="button-container"
+          alignItems="center"
         >
-          Sign up
-        </Button>
+          <Button
+            bg="transparent"
+            border="1px solid"
+            py="20px"
+            px="30px"
+            borderRadius="25px"
+            fontWeight="thin"
+            onClick={() => {
+              navigate('/projects');
+            }}
+            _hover={{
+              bgColor: '#e2e2e2',
+            }}
+            _active={{
+              bgColor: '#e2e2e2',
+            }}
+          >
+            My explore
+          </Button>
 
-        <Button border="1px solid" p="23px" borderRadius="25px">
-          Log in
-        </Button>
-      </Box>
+          <Button
+            border="1px solid #0c0c0c"
+            py="20px"
+            px="30px"
+            borderRadius="25px"
+            color="#fafafa"
+            bgColor="#0c0c0c"
+            fontWeight="regular"
+            _hover={{
+              bgColor: '#2e2e2e',
+            }}
+            _active={{
+              bgColor: '#2e2e2e',
+            }}
+          >
+            My projects
+          </Button>
+
+          <Image w="55px" borderRadius="50%" src={value?.photo_url} />
+        </Flex>
+      ) : (
+        <Box className="button-container">
+          <Button border="1px solid" p="23px" borderRadius="25px">
+            Post a project
+          </Button>
+
+          <Button
+            border="1px solid"
+            p="23px"
+            borderRadius="25px"
+            color="#fafafa"
+            bgColor="#0c0c0c"
+            fontWeight="regular"
+            _hover={{
+              backgroundPosition: 'left bottom',
+            }}
+            _active={{
+              backgroundPosition: 'left bottom',
+            }}
+          >
+            Sign up
+          </Button>
+
+          <Button border="1px solid" p="23px" borderRadius="25px">
+            Log in
+          </Button>
+        </Box>
+      )}
     </Box>
   );
 };
