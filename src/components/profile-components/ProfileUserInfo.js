1fd189516384984879b2b9f2d616f97a250defb3
byronwang93 <byronwang123919@gmail.com>
2024-06-08 23:39:19 -0700

    finished profile page initial info

--- src/components/profile-components/ProfileUserInfo.js
+++ src/components/profile-components/ProfileUserInfo.js
@@ -1,6 +1,10 @@
 import { Button, Flex, HStack, Image, Text, VStack } from '@chakra-ui/react';
 import React from 'react';
 import StupaidVerified from '../global-components/StupaidVerified';
+import LocationIcon from '../../assets/images/location-icon.svg';
+import ProjectPrefIcon from '../../assets/images/project-pref-icon.svg';
+import SchoolIcon from '../../assets/images/school-icon.svg';
+import EditIcon from '../../assets/images/edit-icon.svg';
 
 const ProfileUserInfo = ({ userData, canEdit }) => {
   const {
@@ -21,13 +25,36 @@ const ProfileUserInfo = ({ userData, canEdit }) => {
             {input_name}
           </Text>
           {isVerified && <StupaidVerified />}
-          {canEdit && (
-            <Button>
-              <Text>Edit profile</Text>
-            </Button>
-          )}
         </VStack>
+        {canEdit && (
+          <Button
+            border="#545454 solid 1.5px"
+            bg="transparent"
+            borderRadius="20px"
+            alignSelf="baseline"
+            ml="auto"
+          >
+            <Image mr="5px" src={EditIcon} />
+            <Text>Edit profile</Text>
+          </Button>
+        )}
       </HStack>
+      <VStack mt="30px" color="#545454" alignItems="baseline" w="fit-content">
+        <HStack>
+          <Image src={LocationIcon} />
+          <Text>
+            {city}, {country}
+          </Text>
+        </HStack>
+        <HStack>
+          <Image src={SchoolIcon} />
+          <Text>{school}</Text>
+        </HStack>
+        <HStack>
+          <Image src={ProjectPrefIcon} />
+          <Text>{projectPref}</Text>
+        </HStack>
+      </VStack>
     </Flex>
   );
 };
