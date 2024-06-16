3a637961f651e673778d3a581ca40be745810bb1
Alan Wang <alanwang123919@gmail.com>
2024-06-15 22:25:23 -0700

    create a project page

--- src/components/organization-view-components/NewProject.js
+++ src/components/organization-view-components/NewProject.js
@@ -1,51 +1,163 @@
-import { Button, Flex, Text } from '@chakra-ui/react';
-import React from 'react';
+import { Button, Flex, Input, Text } from '@chakra-ui/react';
+import React, { useState } from 'react';
+
+
 
 const NewProject = () => {
+
+  const [projectName, setProjectName] = useState("");
+  const [projectDescription, setProjectDescription] = useState("");
+  const [projectDeadline, setProjectDeadline] = useState("");
+  const [projectBudget, setProjectBudget] = useState("");
+  const [example, setExample] = useState("");
+  const [optionalNote, setOptionalNote] = useState("");
+
   return (
     <Flex
       backgroundColor="#fafafa"
       width="100vw"
       height="100vh"
       flexDirection="column"
+      pl="60px"
     >
-      <Text>
+      <Text
+        mt="30px"
+        fontSize="24px"
+      >
         Create a project
       </Text>
       
-      <Text>
+      <Text
+        fontWeight="600"
+      >
         Project name*
       </Text>
+      <Input
+        placeholder="e.g. Store banner design"
+        borderRadius="10px"
+        value={projectName}
+        onChange={(e) => setProjectName(e.target.value)}
+        _placeholder={{ color: '#969696' }}
+      />
 
-      <Text>
+      <Text
+        fontWeight="600"
+      >
         Project description*
       </Text>
+      <Input
+        placeholder="Please provide 1-3 lines explaining the project"
+        borderRadius="10px"
+        value={projectDescription}
+        onChange={(e) => setProjectDescription(e.target.value)}
+        _placeholder={{ color: '#969696' }}
+      />
 
-      <Text>
-        Project deadline*
-      </Text>
+      <Flex>
+        <Flex
+          flexDirection="column"
+          w="50%"
+        >
+          <Text
+            fontWeight="600"
+          >
+            Project deadline*
+          </Text>
+          <Input
+            placeholder="*DATE SELECT TEMPLATE*"
+            borderRadius="10px"
+            value={projectDeadline}
+            onChange={(e) => setProjectDeadline(e.target.value)}
+            _placeholder={{ color: '#969696' }}
+          />
+        </Flex>
+        <Flex
+          flexDirection="column"
+        >
+          <Text
+            fontWeight="600"
+          >
+            Project budget (CAD)*
+          </Text>
+          <Input
+            placeholder="Provide a budget of the entire project - we will help break it down between deliverables"
+            borderRadius="10px"
+            value={projectBudget}
+            onChange={(e) => setProjectBudget(e.target.value)}
+            _placeholder={{ color: '#969696' }}
+          />
+        </Flex>
+      </Flex>
 
-      <Text>
-        Project budget (CAD)*
-      </Text>
-
-      <Text>
+      <Text
+        fontWeight="600"
+      >
         Examples / References*
       </Text>
+      <Input
+        placeholder="Provide links of similar work or desired outcome of project"
+        borderRadius="10px"
+        value={example}
+        onChange={(e) => setExample(e.target.value)}
+        _placeholder={{ color: '#969696' }}
+      />
 
-      <Text>
-        Location*
+      <Text
+        fontWeight="600"
+      >
+        Skills*
       </Text>
 
-      <Text>
-        Project preference*
-      </Text>
+      <Flex>
+        <Flex
+          flexDirection="column"
+        >
+          <Text
+            fontWeight="600"
+          >
+            Location*
+          </Text>
+        </Flex>
+        <Flex
+          flexDirection="column"
+        >
+          <Text
+            fontWeight="600"
+          >
+            Project preference*
+          </Text>
+        </Flex>
+      </Flex>
 
-      <Text>
+      <Text
+        fontWeight="600"
+      >
         Optional note
       </Text>
+      <Input
+        placeholder="Any additional information you wish for the creator to know?"
+        borderRadius="10px"
+        value={optionalNote}
+        onChange={(e) => setOptionalNote(e.target.value)}
+        _placeholder={{ color: '#969696' }}
+      />
 
-      <Button>
+      <Button
+        width="100%"
+        border="1px solid"
+        p="4px 10px"
+        mt="30px"
+        borderRadius="25px"
+        color="#fafafa"
+        bgColor="#0c0c0c"
+        fontWeight="300"
+        _hover={{
+          backgroundPosition: 'left bottom',
+        }}
+        _active={{
+          backgroundPosition: 'left bottom',
+        }}
+      >
         Create project
       </Button>
     </Flex>
