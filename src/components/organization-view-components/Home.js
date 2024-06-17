c835fa6fea7d070dcf385aab2b9543787b373122
byronwang93 <byronwang123919@gmail.com>
2024-06-17 15:32:01 -0700

    refactor

--- src/components/organization-view-components/Home.js
+++ src/components/organization-view-components/Home.js
@@ -5,7 +5,6 @@ import Sidebar from './Sidebar';
 import Creatives from './Creatives';
 
 const Home = () => {
-
   const [filters, setFilters] = useState({});
 
   const handleFilterChange = (newFilters) => {
@@ -16,7 +15,8 @@ const Home = () => {
     <Box
       backgroundColor="#fafafa"
       width="100vw"
-      height="100vh"
+      height="fitContent"
+      minH="100vh"
     >
       <Navbar />
       <Box display="flex" flexDirection="row" justifyContent="space-between">
