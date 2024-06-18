93551ba93927c9588c49d62dd76f50b14d13c443
byronwang93 <byronwang123919@gmail.com>
2024-06-17 17:18:51 -0700

    fixed navbar for non-logged in organizations

--- src/components/global-components/Navbar.js
+++ src/components/global-components/Navbar.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-// import StupaidLogo from './../../assets/images/stupaid-logo.svg';
 import StupaidLogo from './../../assets/landing-page-images/stupaid-logo-main.svg';
 import DefaultProfile from '../../assets/images/default-pfp.svg';
 import {
@@ -54,12 +53,14 @@ const Navbar = () => {
           w={{ base: '150px', mdLg: '120px' }}
           src={StupaidLogo}
         />
-        <Text fontWeight="thin" fontSize="20px" color="#555555">
-          Hi,{' '}
-          <Box as="span" textDecoration="underline 1.3px">
-            {value === {} ? 'test_user' : value?.name}
-          </Box>
-        </Text>
+        {value?.name && (
+          <Text fontWeight="thin" fontSize="20px" color="#555555">
+            Hi,{' '}
+            <Box as="span" textDecoration="underline 1.3px">
+              {value === {} ? 'test_user' : value.name}
+            </Box>
+          </Text>
+        )}
       </Flex>
 
       {value?.type === 'student' ? (
@@ -108,16 +109,7 @@ const Navbar = () => {
           </Button>
 
           <Menu>
-            <MenuButton
-            // as={
-            //   <Image
-            //     _hover={{ cursor: 'pointer' }}
-            //     w="55px"
-            //     borderRadius="50%"
-            //     src={value?.photo_url}
-            //   />
-            // }
-            >
+            <MenuButton>
               <Box
                 borderRadius="100%"
                 w="60px"
@@ -132,58 +124,72 @@ const Navbar = () => {
                 position="relative"
                 align="center"
               ></Box>
-              {/* <Image
-                _hover={{ cursor: 'pointer' }}
-                w="55px"
-                borderRadius="50%"
-                src={value?.photo_url ? value.photo_url : DefaultProfile}
-              /> */}
             </MenuButton>
             <MenuList>
               <MenuItem onClick={goToProfile}>My profile</MenuItem>
               <MenuItem onClick={logout}>Log out</MenuItem>
             </MenuList>
           </Menu>
-          {/* <Image
-            _hover={{ cursor: 'pointer' }}
-            w="55px"
-            borderRadius="50%"
-            src={value?.photo_url}
-          /> */}
         </Flex>
       ) : (
-        <Box className="button-container">
-          <Button border="1px solid" p="23px" borderRadius="25px"
-            onClick={createProject}>
+        <Flex gap="10px" className="button-container">
+          <Button
+            p="23px"
+            bg="transparent"
+            border="1px solid"
+            py="20px"
+            px="30px"
+            borderRadius="25px"
+            fontWeight="thin"
+            _hover={{
+              bgColor: '#e2e2e2',
+            }}
+            _active={{
+              bgColor: '#e2e2e2',
+            }}
+            onClick={createProject}
+          >
             Post a project
           </Button>
 
           <Button
-            border="1px solid"
-            p="23px"
             borderRadius="25px"
             color="#fafafa"
             bgColor="#0c0c0c"
             fontWeight="regular"
+            border="1px solid #0c0c0c"
+            py="20px"
+            px="25px"
             _hover={{
-              backgroundPosition: 'left bottom',
+              bgColor: '#2e2e2e',
             }}
             _active={{
-              backgroundPosition: 'left bottom',
+              bgColor: '#2e2e2e',
             }}
+            onClick={login}
           >
             Sign up
           </Button>
 
           <Button
             onClick={login}
-            border="1px solid"
             p="23px"
+            bg="transparent"
+            border="1px solid"
+            py="20px"
+            px="30px"
             borderRadius="25px"
+            fontWeight="thin"
+            _hover={{
+              bgColor: '#e2e2e2',
+            }}
+            _active={{
+              bgColor: '#e2e2e2',
+            }}
           >
             Log in
           </Button>
-        </Box>
+        </Flex>
       )}
     </Box>
   );
