657b4a6a8903f3b86a83be16ed4732371bc6f1b8
Alan Wang <alanwang123919@gmail.com>
2024-06-30 21:06:59 -0700

    inital setup for socket.io

--- src/components/messaging/Messaging.js
+++ src/components/messaging/Messaging.js
@@ -1,11 +1,40 @@
-import { Button, Input } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Button, Input } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+const socket = io.connect("http://localhost:3001");
 
 const Messaging = () => {
+
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const sendMessage = () => {
+    socket.emit("send_message", {
+      message
+    });
+  };
+
+  useEffect(() => {
+    socket.on("received_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
   return (
     <div>
-      <Input placeholder='Enter your message here' textColor='white'/>
-      <Button>Submit</Button>
+      <Input placeholder='Enter your message here' textColor='white' 
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <Button onClick={sendMessage}>Submit</Button>
+
+      <Box
+        backgroundColor='white'
+      >
+        <h1>Message:</h1>
+        {messageReceived}
+      </Box>
     </div>
   );
 };
