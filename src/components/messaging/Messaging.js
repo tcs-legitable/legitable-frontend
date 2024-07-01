2e34d9bc2c14ae9f34ef36a1ca6c6020377b7d0a
Alan Wang <alanwang123919@gmail.com>
2024-06-30 21:23:00 -0700

    added room joining in socket.io

--- src/components/messaging/Messaging.js
+++ src/components/messaging/Messaging.js
@@ -5,23 +5,36 @@ const socket = io.connect("http://localhost:3001");
 
 const Messaging = () => {
 
+  const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
   const [messageReceived, setMessageReceived] = useState("");
 
+  const joinRoom = () => {
+    if (room !== "") {
+      socket.emit("join_room", room);
+    }
+  };
+
   const sendMessage = () => {
     socket.emit("send_message", {
-      message
+      message, room
     });
   };
 
   useEffect(() => {
-    socket.on("received_message", (data) => {
+    socket.on("receive_message", (data) => {
       setMessageReceived(data.message);
     });
   }, [socket]);
 
   return (
     <div>
+      <Input placeholder='Enter room number' textColor='white'
+        onChange={(event) => {
+          setRoom(event.target.value);
+        }}
+      />
+      <Button onClick={joinRoom}>Join room</Button>
       <Input placeholder='Enter your message here' textColor='white' 
         onChange={(event) => {
           setMessage(event.target.value);
