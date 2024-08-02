b6b12472c617f0e7989bbd8ef1832cacd6bf05a1
byronwang93 <byronwang123919@gmail.com>
2024-08-02 10:49:13 -0700

    removed bug message

--- src/components/messaging/Messaging.js
+++ src/components/messaging/Messaging.js
@@ -8,7 +8,7 @@ import MessagingHeader from './MessagingHeader';
 import SendMessage from './SendMessage';
 import defaultProfilePic from './../../assets/landing-page-images/stupaid-logo-main.svg';
 
-const socket = io.connect("https://legitable-backend.up.railway.app/");
+const socket = io.connect('https://legitable-backend.up.railway.app/');
 // const socket = io.connect("http://localhost:3001");
 
 const Messaging = () => {
@@ -17,11 +17,12 @@ const Messaging = () => {
   const [messages, setMessages] = useState([]);
   const messagesEndRef = useRef(null);
 
-  const roomId = user1 && user2 ? `room-${[user1, user2].sort().join('-')}` : null;
+  const roomId =
+    user1 && user2 ? `room-${[user1, user2].sort().join('-')}` : null;
 
   const joinRoom = async () => {
     if (roomId) {
-      socket.emit("join_room", roomId);
+      socket.emit('join_room', roomId);
       const initialMessages = await getMessages(roomId);
       setMessages(initialMessages);
     }
@@ -34,14 +35,14 @@ const Messaging = () => {
   useEffect(() => {
     if (roomId) {
       joinRoom();
-      socket.on("receive_message", (data) => {
-        console.log("Received message:", data);
+      socket.on('receive_message', (data) => {
+        console.log('Received message:', data);
         setMessages((prevMessages) => [...prevMessages, data]);
       });
     }
 
     return () => {
-      socket.off("receive_message");
+      socket.off('receive_message');
     };
   }, [roomId]);
 
@@ -51,9 +52,17 @@ const Messaging = () => {
 
   if (!roomId) {
     return (
-      <Flex flexDirection="column" backgroundColor="#fafafa" w="100%" h="100vh" alignItems="center" justifyContent="center">
-        <Text textAlign='center' width='50%' color='#8c8c8c'>
-          Come chat with us by clicking the profile on the left! If you have any suggestions or find any bugs, our DM's are always open!
+      <Flex
+        flexDirection="column"
+        backgroundColor="#fafafa"
+        w="100%"
+        h="100vh"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Text textAlign="center" width="50%" color="#8c8c8c">
+          Come chat with us by clicking the profile on the left! If you have any
+          suggestions or find any bugs, our DM's are always open!
         </Text>
       </Flex>
     );
@@ -62,43 +71,40 @@ const Messaging = () => {
   return (
     <Flex flexDirection="column" backgroundColor="#fafafa" w="100%" h="100vh">
       <MessagingHeader />
-      
+
       <Box backgroundColor="#fafafa" p="20px" overflowY="auto" flex="1">
-        <Flex w="100%" justifyContent="center">
-          <Text
-            textAlign='center'
-            width='50%'
-            color='#8c8c8c'
-          >
-            If you have any suggestions or find any bugs, our DM's are always open below!
-          </Text>
-        </Flex>
         {messages.map((msg, index) => {
           const previousMessage = messages[index - 1];
-          const isSameAuthor = previousMessage && previousMessage.author === msg.author;
+          const isSameAuthor =
+            previousMessage && previousMessage.author === msg.author;
           const nextMessage = messages[index + 1];
-          const isLastFromAuthor = !nextMessage || nextMessage.author !== msg.author;
-          const showProfilePicture = msg.author !== value.name && isLastFromAuthor;
-          const marginTop = isSameAuthor ? "5px" : "20px";
-          
+          const isLastFromAuthor =
+            !nextMessage || nextMessage.author !== msg.author;
+          const showProfilePicture =
+            msg.author !== value.name && isLastFromAuthor;
+          const marginTop = isSameAuthor ? '5px' : '20px';
+
           const isCurrentUser = msg.author === value.name;
 
           return (
             <Box key={index} mt={marginTop}>
               {!isCurrentUser && !isSameAuthor && (
-                <Text mb='2px' ml='55px' color='#8c8c8c'>
+                <Text mb="2px" ml="55px" color="#8c8c8c">
                   {msg.author}
                 </Text>
               )}
-              <Flex align="flex-end" justify={isCurrentUser ? 'flex-end' : 'flex-start'}>
+              <Flex
+                align="flex-end"
+                justify={isCurrentUser ? 'flex-end' : 'flex-start'}
+              >
                 {!isCurrentUser && (
                   <Box w="50px" minW="50px">
                     {showProfilePicture && (
-                      <Image 
-                        src={defaultProfilePic} 
-                        borderRadius="full" 
-                        boxSize="40px" 
-                        mr="10px" 
+                      <Image
+                        src={defaultProfilePic}
+                        borderRadius="full"
+                        boxSize="40px"
+                        mr="10px"
                       />
                     )}
                   </Box>
@@ -113,12 +119,17 @@ const Messaging = () => {
                   sx={{
                     ':hover .message-time': {
                       opacity: 1,
-                    }
+                    },
                   }}
                 >
                   <p>{msg.message}</p>
                   {msg.fileURL && (
-                    <Link href={msg.fileURL} isExternal color={isCurrentUser ? "#63b3ed" : "blue.500"} textDecoration="underline">
+                    <Link
+                      href={msg.fileURL}
+                      isExternal
+                      color={isCurrentUser ? '#63b3ed' : 'blue.500'}
+                      textDecoration="underline"
+                    >
                       {msg.fileName}
                     </Link>
                   )}
